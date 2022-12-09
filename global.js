1d0d3602bbff26c3728cd8b3b3062e0a518a2905
Luke <Luke@zezamii.com>
2022-12-09 13:18:13 +1000

    changing the file structure to work with webflow.

---
+++ global.js
@@ -0,0 +1,31 @@
+var webFlowAuth = {
+  loginPath: "/log-in",
+  loginRedirectPath: "/dashboard",
+  signupPath: "/sign-up",
+  signupRedirectPath: "/dashboard",
+  logoutRedirectPath: "/",
+  firebaseConfig: {
+    apiKey: "AIzaSyASFSsHovAAFE6bKzPt2pprKFa1pF7FzYs",
+    authDomain: "zezamiittlockintegration.firebaseapp.com",
+    projectId: "zezamiittlockintegration",
+    storageBucket: "zezamiittlockintegration.appspot.com",
+    messagingSenderId: "399470193827",
+    appId: "1:399470193827:web:2da9152802f29d3b9e2316",
+    measurementId: "G-3JX478JX8X",
+  },
+};
+// initialise firebase Auth & Firestore
+firebase.initializeApp(webFlowAuth.firebaseConfig);
+const db = firebase.firestore();
+
+// var logoutButton = document.getElementById("logoutButton");
+// if (typeof logoutButton !== null) {
+//   logoutButton.addEventListener("click", handleSignOut);
+// } else {
+// }
+
+// function handleSignOut() {
+//   window.location.replace("./log-in");
+//   console.log("user signed out");
+//   // Sign-out successful.
+// }
--- test.js
+++
@@ -1,7 +0,0 @@
-const testing = () => {
-  console.log("this works.");
-};
-
-function anothertest(kishan) {
-  console.log(kishan);
-}
